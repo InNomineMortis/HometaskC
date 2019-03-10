12ebbcef82f36e6635cb95493a747d6087464aee
Dmitry <dmpuchenkov@protonmail.com>
2019-03-10 15:24:01 +0300

    final version without valgrind check

--- HometaskA/main.c
+++ HometaskA/main.c
@@ -8,6 +8,8 @@ char *GetLine();
 
 char *ShiftTen(int SecondBase, char *str);
 
+char *ShiftTenInt(int SecondBase, int Number);
+
 char Numbers[36] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int main() {
@@ -15,7 +17,10 @@ int main() {
     int Q; // Output number system
     scanf("%d%d", &P, &Q);
     char *str = Shift(P, Q, GetLine());
-    for (int i = (int) (strlen(str) - 1); i >= 0; i--)
+    int i = 0;
+    while (str[i] != '\0')
+        i++;
+    for (i = i - 1; i >= 0; i--)
         printf("%c", str[i]);
     return 0;
 }
@@ -25,12 +30,14 @@ char *GetLine() {
     const size_t Incr = 32;
     char Buffer[Length];
     char *FinalInp = NULL;
-    size_t NumberCharacters = 0;
+    int NumberCharacters = 0;
     size_t CurrentInp = 0;
     size_t BestInp = 0;
-
+    int i = 0;
     while (fgets(Buffer, Length, stdin)) {
-        NumberCharacters = strlen(Buffer);
+        while (Buffer[i] != '\0')
+            i++;
+        NumberCharacters = i;
         CurrentInp += NumberCharacters;
         if (Buffer[NumberCharacters - 1] == '\n') {
             Buffer[NumberCharacters - 1] = '\0';
@@ -60,26 +67,50 @@ char *Shift(int FirstBase, int SecondBase, char *str) {
     if (FirstBase == 10)
         Num = ShiftTen(SecondBase, str);
     else {
-        char Buffer[32];
-        Buffer[0] = '\0';
+        int Buffer = 0;
         int NumberCharacters = 0;
-        size_t BestInp = 0;
-        int current = 0, j = 0;
-        char *NewStr = NULL;
-        for (size_t i = strlen(str) - 1; i > 0; i--) {
-            if (NumberCharacters >= BestInp) {
-                BestInp += 32;
-                NewStr = realloc(NewStr, BestInp);
-                strcat(NewStr, Buffer);
-                j = 0;
+        int current = 0;
+        int Dec = 0;
+        int i = 0;                                      //Количество элементов без strlen
+        while (str[i] != '\0')
+            i++;
+        for (int x = i - 1; x >= 0; x--) {
+            Dec = str[x];
+            if ((47 < Dec) && (Dec < 58))
+                Dec -= 48;
+            else
+                Dec -= 60;
+            if (Dec >= 0) {
+                current = Dec;
+                while (NumberCharacters > 0) {
+                    current *= FirstBase;
+                    NumberCharacters--;
+                }
+                Buffer += current;
+                NumberCharacters++;
             }
-            current = (int) str[i] * (FirstBase ^ NumberCharacters);
-            Buffer[j] += current;
-            NumberCharacters++;
-            j++;
         }
-        strcat(NewStr, Buffer);
-        Num = ShiftTen(SecondBase, Num);
+        if (SecondBase != 10)
+            Num = ShiftTenInt(SecondBase, Buffer);
+        else {
+            i = 0;
+            int numeral = Buffer;
+            while (numeral > 0) {
+                i++;
+                numeral /= 10;
+            }
+            char Numb[i + 1];
+            int z = 0;
+            while (Buffer > 0) {
+                numeral = Buffer % 10;
+                Numb[z] = (char) (numeral + 48);
+                Buffer /= 10;
+                z++;
+            }
+            Numb[i] = '\0';
+            Num = realloc(Num, (i + 1));
+            Num = Numb;
+        }
     }
     return Num;
 }
@@ -105,6 +136,33 @@ char *ShiftTen(int SecondBase, char *str) {
         i++;
     }
     Buff[i] = Numbers[NewNum];
+    strcat(Num, Buff);
+    return Num;
+}
+
+char *ShiftTenInt(int SecondBase, int Number) {
+    char *Num = NULL;
+    char Buff[32];
+    Buff[0] = '\0';
+    int NumberCharacters = 0;
+    size_t BestInp = 0;
+    int i = 0;
+    int inp = 0;
+    while (Number >= SecondBase) {
+        if (NumberCharacters >= BestInp) {
+            BestInp += 32;
+            Num = realloc(Num, BestInp);
+            strcat(Num, Buff);
+            i = 0;
+        }
+        inp = Number % SecondBase;
+        Buff[i] = Numbers[inp];
+        Number /= SecondBase;
+        i++;
+        NumberCharacters++;
+    }
+    Buff[i] = Numbers[Number];
+    Buff[i + 1] = '\0';
     strcat(Num, Buff);
     return Num;
 }
